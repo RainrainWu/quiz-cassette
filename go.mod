2f6c9a5930e69b678208006e2da2f795142e38c8
RainrainWu <s0958334772@gmail.com>
2021-06-03 15:40:29 +0800

    feat(init): init go mod

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/RainrainWu/quizdeck
+
+go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0
+	go.uber.org/zap v1.17.0 // indirect
+)
---
+++ logger.go
@@ -0,0 +1,14 @@
+package quizdeck
+
+import (
+	"go.uber.org/zap"
+)
+
+var (
+	Logger *zap.Logger = NewLogger()
+)
+
+func NewLogger() *zap.Logger {
+	logger, _ := zap.NewProduction()
+	return logger
+}
